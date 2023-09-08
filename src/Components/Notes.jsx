4fc8643c8fe95774b09caab114fc09464150381c
Adwaith Nair <adwaith1310@gmail.com>
2023-09-08 14:33:16 +0530

    added notes section

--- src/Components/Notes.jsx
+++ src/Components/Notes.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 import "./../CSS/Notes.css";
 import "./../CSS/Popup.css";
@@ -9,13 +9,11 @@ const customStyles = {
     left: "50%",
     right: "auto",
     bottom: "auto",
-    width:'35%',
-    height:'31%',
+    width: "35%",
+    height: "31%",
     transform: "translate(-50%, -50%)",
   },
 };
-
-Modal.setAppElement("#root");
 
 export default function Notes() {
   const [modalIsOpen, setIsOpen] = useState(false);
@@ -23,7 +21,7 @@ export default function Notes() {
   function openModal() {
     setIsOpen(true);
   }
-  
+
   function closeModal() {
     setIsOpen(false);
   }
@@ -55,18 +53,33 @@ export default function Notes() {
     },
   ]);
 
-  const [inputText, setInputText] = useState('');
-  const [displayText, setDisplayText] = useState([]);
-  
-  const handleInputChange = (e) => {
-    setInputText(e.target.value);
-  };
+  const [inputText, setInputText] = useState("");
+  const [inputData, setInputData] = useState([]);
+  const [selectedColor, setSelectedColor] = useState(null);
+  const [selectedGroup, setSelectedGroup] = useState(null);
 
   const addTextToDisplay = (e) => {
-    e.preventDefault()
-      setDisplayText([...displayText, inputText]);
-      localStorage.setItem('groups',displayText)
-      setInputText('');
+    e.preventDefault();
+    const groups = { text: inputText, color: selectedColor,notes:[] };
+    const updatedData = [...inputData, groups];
+    setInputData(updatedData);
+    localStorage.setItem("groups", JSON.stringify(updatedData));
+    setInputText("");
+  };
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("groups"));
+    if (storedData) {
+      setInputData(storedData);
+    }
+  }, []);
+
+  const handleSelectedColor = (color) => {
+    setSelectedColor(color);
+  };
+
+  const handleSelectedGroup = (index) => {
+    setSelectedGroup(index);
   };
 
   return (
@@ -82,66 +95,96 @@ export default function Notes() {
             ></img>
             Create Notes Group
           </button>
+
           <div className="popup-window">
-            <Modal
-              isOpen={modalIsOpen}
-              onRequestClose={closeModal}
-              style={customStyles}
-            >
+            <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}>
               <form>
-              <h1 className="popup-heading">Create New Notes Group</h1>
-              <span className="group-name-text">Group Name</span>
-              <input
-                placeholder="Enter your group name..."
-                className="group-name-input"
-                onChange={handleInputChange}
-              ></input>
-              <div className="color-section">
-              <span className="choose-color-text">Choose Color</span>
-              <div className="choose-colors">
-                {colors.map((color) => (
-                  <div className="colors"
-                    key={color.id}
-                    style={{
-                      backgroundColor: color.color,
-                    }}
-                  ></div>
-                ))}
-              </div>
-              </div>
-              <button className="create-btn" onClick={addTextToDisplay} >Create</button>
-            </form>
+                <h1 className="popup-heading">Create New Notes Group</h1>
+                <span className="group-name-text">Group Name</span>
+                <input
+                  placeholder="Enter your group name..."
+                  className="group-name-input"
+                  onChange={(e) => setInputText(e.target.value)}
+                  value={inputText}
+                ></input>
+                <div className="color-section">
+                  <span className="choose-color-text">Choose Color</span>
+                  <div className="choose-colors">
+                    {colors.map((color) => (
+                      <div
+                        className={`colors ${
+                          selectedColor === color ? "selected" : ""
+                        }`}
+                        key={color.id}
+                        style={{ backgroundColor: color.color }}
+                        onClick={() => handleSelectedColor(color)}
+                      ></div>
+                    ))}
+                  </div>
+                </div>
+                <button className="create-btn" onClick={addTextToDisplay}>
+                  Create
+                </button>
+              </form>
             </Modal>
           </div>
 
           <div className="groups-display">
-          {displayText.map((input, index) => (
-          <div key={index}>{input}</div>
-        ))}
+            {inputData.map((input, index) => (
+              <div key={index} className="group-container" onClick={() => handleSelectedGroup(index)}>
+                <div className="profile" style={{ backgroundColor: input.color.color }}>
+                  {input.text.replace().slice(0, 2).toUpperCase()}
+                </div>
+                <div className="group-text">{input.text}</div>
+              </div>
+            ))}
           </div>
         </div>
 
         <div className="initial-display">
-          <img
-            src="./icons/logo.png"
-            alt="display-icon"
-            className="display-logo"
-          ></img>
-          <h1 className="display-heading">Pocket Notes</h1>
-          <p className="display-text">
-            Send and receive messages without keeping your phone online.
-            <br></br>
-            Use Pocket Notes on up to 4 linked devices and 1 mobile phone
-          </p>
-          <footer className="encrypt-text">
+          {selectedGroup !== null && (
+          <div className="notes-taking-section">
+          <div className="notes-section">
+            <div className="profile-section">
+            <div className="notes-profile" style={{ backgroundColor: inputData[selectedGroup].color.color }}>
+              {inputData[selectedGroup].text.replace().slice(0,2).toUpperCase()}
+            </div>
+            <h2 className="notes-group-name">{inputData[selectedGroup].text}</h2>
+            </div>
+            <div className="notes-display">
+
+            </div>
+          <div className="textarea">
+            <textarea className="notes-input" placeholder="Enter your text here..." ></textarea>
+            </div>
+          </div>
+          </div>
+        )}
+
+        {selectedGroup === null && (
+          <div>
             <img
-              src="./icons/lock.svg"
-              alt="encryption-logo"
-              className="lock-icon"
+              src="./icons/logo.png"
+              alt="display-icon"
+              className="display-logo"
             ></img>
-            <footer className="footer">end-to-end encrypted</footer>
-          </footer>
-        </div>
+            <h1 className="display-heading">Pocket Notes</h1>
+            <p className="display-text">
+              Send and receive messages without keeping your phone online.
+              <br></br>
+              Use Pocket Notes on up to 4 linked devices and 1 mobile phone
+            </p>
+            <footer className="encrypt-text">
+              <img
+                src="./icons/lock.svg"
+                alt="encryption-logo"
+                className="lock-icon"
+              ></img>
+              <footer className="footer">end-to-end encrypted</footer>
+            </footer>
+          </div>
+        )}
+      </div>
       </div>
     </>
   );
